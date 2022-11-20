7eec7352a3c62a693fae2fbaf61eccfe56919265
Zivotovskiy <https://github.com/ZhivotovskyMaksim>
2022-11-21 03:12:12 +0500

    step_2

--- InternetMarket/src/main/java/com/max/exempl/controller/ProductController.java
+++ InternetMarket/src/main/java/com/max/exempl/controller/ProductController.java
@@ -1,14 +1,42 @@
 package com.max.exempl.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.max.exempl.model.Product;
+import com.max.exempl.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
 
-	@GetMapping("/")
-	public String products() {
-		return "products";
-	}
+    @GetMapping("/")
+    public String products(Model model) {
+        model.addAttribute("products", productService.listProducts());
+        return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-info";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/";
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/";
+    }
 }
---
+++ InternetMarket/src/main/java/com/max/exempl/model/Product.java
@@ -0,0 +1,15 @@
+package com.max.exempl.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String title;
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+}
